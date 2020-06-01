5d85d1fabbfbfb44ccde5764aa70b07a8f3fa33e
Max Shcherbinin <maksimserbinin9119@gmail.com>
2020-06-01 14:42:43 +0300

    Update index.blade.php

--- resources/views/client/layouts/index.blade.php
+++ resources/views/client/layouts/index.blade.php
@@ -177,9 +177,6 @@
 
         <section class="ftco-about d-md-flex bg-light">
             <div class="one-half img" style="background-image: url(/images/about-2.jpg);">
-                <a href="https://vimeo.com/45830194" class="icon popup-vimeo d-flex justify-content-center align-items-center">
-                    <span class="icon-play"></a>
-                </a>
             </div>
             <div class="one-half ">
                 <div class="heading-section ">
@@ -695,7 +692,7 @@
             <div class="overlay"></div>
             <div class="container">
                 <div class="row mb-5">
-                    <div class="col-lg-3 col-md-6 mb-5 mb-md-5">
+                    <div class="col-lg-3 col-md-3 mb-5 mb-md-5">
                         <div class="ftco-footer-widget mb-4">
                             <h2 class="ftco-heading-2">About Us</h2>
                             <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
@@ -706,53 +703,30 @@
                             </ul>
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-6 mb-5 mb-md-5">
-                        <div class="ftco-footer-widget mb-4">
-                            <h2 class="ftco-heading-2">Recent Blog</h2>
-                            <div class="block-21 mb-4 d-flex">
-                                <a class="blog-img mr-4" style="background-image: url(/images/image_1.jpg);"></a>
-                                <div class="text">
-                                    <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>
-                                    <div class="meta">
-                                        <div><a href="#"><span class="icon-calendar"></span> Dec 25, 2018</a></div>
-                                        <div><a href="#"><span class="icon-person"></span> Admin</a></div>
-                                        <div><a href="#"><span class="icon-chat"></span> 19</a></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="block-21 mb-4 d-flex">
-                                <a class="blog-img mr-4" style="background-image: url(/images/image_2.jpg);"></a>
-                                <div class="text">
-                                    <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>
-                                    <div class="meta">
-                                        <div><a href="#"><span class="icon-calendar"></span> Dec 25, 2018</a></div>
-                                        <div><a href="#"><span class="icon-person"></span> Admin</a></div>
-                                        <div><a href="#"><span class="icon-chat"></span> 19</a></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-6 mb-5 mb-md-5">
-                        <div class="ftco-footer-widget mb-4 ml-md-4">
-                            <h2 class="ftco-heading-2">Services</h2>
-                            <ul class="list-unstyled">
-                                <li><a href="#" class="py-2 d-block">Boost Your Body</a></li>
-                                <li><a href="#" class="py-2 d-block">Achieve Your Goal</a></li>
-                                <li><a href="#" class="py-2 d-block">Analyze Your Goal</a></li>
-                                <li><a href="#" class="py-2 d-block">Improve Your Performance</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 mb-5 mb-md-5">
-                        <div class="ftco-footer-widget mb-4">
-                            <h2 class="ftco-heading-2">Have a Questions?</h2>
-                            <div class="block-23 mb-3">
-                                <ul>
-                                    <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-                                    <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-                                    <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
+                    <div class="col-lg-4 col-md-4 mb-5 mb-md-5">
+                        <div class="ftco-footer-widget mb-4 ml-md-4 d-flex justify-content-end">
+                            <div>
+                                <h2 class="ftco-heading-2">Services</h2>
+                                <ul class="list-unstyled">
+                                    <li><a href="#" class="py-2 d-block">Boost Your Body</a></li>
+                                    <li><a href="#" class="py-2 d-block">Achieve Your Goal</a></li>
+                                    <li><a href="#" class="py-2 d-block">Analyze Your Goal</a></li>
+                                    <li><a href="#" class="py-2 d-block">Improve Your Performance</a></li>
                                 </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-5 col-md-5 mb-5 mb-md-5">
+                        <div class="ftco-footer-widget mb-4 d-flex justify-content-end">
+                            <div class="w-75">
+                                <h2 class="ftco-heading-2">Have a Questions?</h2>
+                                <div class="block-23 mb-3">
+                                    <ul>
+                                        <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
+                                        <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
+                                        <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
